32add9fe45a1599ce8beafbf74ee6ab66dd4c3bc
Josh Carlile <carlilejs@gmail.com>
2020-05-16 11:09:51 -0700

    Solve sort odds

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python sort the odds file
+---
---
+++ sortodds.py
@@ -0,0 +1,18 @@
+# Your task is to sort ascending odd numbers but even numbers must be on their places.
+# Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+
+# sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+
+
+def sort_array(arr):
+	# Sort an array of the odd numbers from original array
+	# Use reverse so that pop() can be used below to pull smallest numbers off the end
+  	odds = sorted([x for x in arr if x % 2 != 0], reverse = True)
+  	# Create and return a new array with odd numbers replaced from the descending odds array
+	return [x if x % 2 == 0 else odds.pop() for x in arr]
+
+arr = [3,2,1,4,4,6,1,8]
+sorted_odds = sort_array(arr)
+
+# print(arr) # [3,2,1,4,4,6,1,8]
+# print(sorted_odds) # [1,2,1,4,4,6,3,8]
